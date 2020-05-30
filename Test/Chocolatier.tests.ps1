98c2a93e6cc8294b6762bd8a9ddddf3efb1bf0ba
ethanbergstrom <ethan.bergtrom@gmail.com>
2020-05-30 10:35:53 -0500

    Fix AppVeyor to stop on test failure

--- Test/Chocolatier.tests.ps1
+++ Test/Chocolatier.tests.ps1
@@ -129,8 +129,8 @@ Describe "$platform multi-source support" {
 }
 
 Describe "$platform version filters" {
-	$package = "cpu-z"
-	$version = "1.77"
+	$package = 'cpu-z'
+	$version = '1.85'
 
 	AfterAll {
 		Uninstall-Package -Name $package -Provider $Chocolatier -ErrorAction SilentlyContinue
